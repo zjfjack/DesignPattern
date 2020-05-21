840af254cde821e9e1338cae560036e4aca19f34
zjfjack <zekejeff@gmail.com>
2020-05-21 23:45:01 +1000

    Fix two adapter patterns identation issue

--- ClassAdapterPattern/ClassAdapterPattern.cpp
+++ ClassAdapterPattern/ClassAdapterPattern.cpp
@@ -5,61 +5,61 @@
 class Duck
 {
 public:
-	virtual ~Duck() = default;
-	virtual void flyDuck()
-	{
-		std::cout << "I'm flying" << std::endl;
-	}
-	virtual void quack() {
-		std::cout << "Quack" << std::endl;
-	}
+    virtual ~Duck() = default;
+    virtual void flyDuck()
+    {
+        std::cout << "I'm flying" << std::endl;
+    }
+    virtual void quack() {
+        std::cout << "Quack" << std::endl;
+    }
 };
 
 // Adaptee concrete class (Adaptee in class adpater pattern has to be concrete class)
 class Turkey
 {
 public:
-	virtual ~Turkey() = default;
-	virtual void flyTurkey()
-	{
-		std::cout << "I'm flying a short distance" << std::endl;
-	}
+    virtual ~Turkey() = default;
+    virtual void flyTurkey()
+    {
+        std::cout << "I'm flying a short distance" << std::endl;
+    }
 
-	virtual void gobble()
-	{
-		std::cout << "Gobble" << std::endl;
-	}
+    virtual void gobble()
+    {
+        std::cout << "Gobble" << std::endl;
+    }
 };
 
 // Adapter is Multiple inheritance from Target and Adaptees
 class TurkeyAdapter : public Duck, private Turkey
 {
 public:
-	virtual void quack() override
-	{
-		Turkey::gobble();
-	}
+    virtual void quack() override
+    {
+        Turkey::gobble();
+    }
 
-	virtual void flyDuck() override
-	{
-		for (int i = 0; i < 5; i++)
-			Turkey::flyTurkey();
-	}
+    virtual void flyDuck() override
+    {
+        for (int i = 0; i < 5; i++)
+            Turkey::flyTurkey();
+    }
 };
 
 void testDuck(const std::unique_ptr<Duck>& duck)
 {
-	duck->flyDuck();
-	duck->quack();
+    duck->flyDuck();
+    duck->quack();
 }
 
 int main()
 {
-	std::unique_ptr<Duck> duck = std::make_unique<Duck>();
-	testDuck(duck);
+    std::unique_ptr<Duck> duck = std::make_unique<Duck>();
+    testDuck(duck);
 
-	std::unique_ptr<Duck> turkeyAdpter = std::make_unique<TurkeyAdapter>();
-	testDuck(turkeyAdpter);
+    std::unique_ptr<Duck> turkeyAdpter = std::make_unique<TurkeyAdapter>();
+    testDuck(turkeyAdpter);
 
-	// std::unique_ptr<Turkey> wrongDuckAdpter = std::make_unique<TurkeyAdapter>(); not allowed because base class Turkey is private // Target is Duck!!!
+    // std::unique_ptr<Turkey> wrongDuckAdpter = std::make_unique<TurkeyAdapter>(); not allowed because base class Turkey is private // Target is Duck!!!
 }
--- ObjectAdapterPattern/ObjectAdapterPattern.cpp
+++ ObjectAdapterPattern/ObjectAdapterPattern.cpp
@@ -5,78 +5,78 @@
 class Duck
 {
 public:
-	virtual ~Duck() = default;
-	virtual void fly() = 0;
-	virtual void quack() = 0;
+    virtual ~Duck() = default;
+    virtual void fly() = 0;
+    virtual void quack() = 0;
 };
 
 class MallardDuck : public Duck
 {
 public:
-	virtual void fly() override
-	{
-		std::cout << "I'm flying" << std::endl;
-	}
-	virtual void quack() override {
-		std::cout << "Quack" << std::endl;
-	}
+    virtual void fly() override
+    {
+        std::cout << "I'm flying" << std::endl;
+    }
+    virtual void quack() override {
+        std::cout << "Quack" << std::endl;
+    }
 };
 
 // Adaptee interface
 class Turkey
 {
 public:
-	virtual ~Turkey() = default;
-	virtual void fly() = 0;
-	virtual void gobble() = 0;
+    virtual ~Turkey() = default;
+    virtual void fly() = 0;
+    virtual void gobble() = 0;
 };
 
 // Adaptee concrete class
 class WildTurkey : public Turkey
 {
 public:
-	virtual void fly() override
-	{
-		std::cout << "I'm flying a short distance" << std::endl;
-	}
+    virtual void fly() override
+    {
+        std::cout << "I'm flying a short distance" << std::endl;
+    }
 
-	virtual void gobble() override
-	{
-		std::cout << "Gobble" << std::endl;
-	}
+    virtual void gobble() override
+    {
+        std::cout << "Gobble" << std::endl;
+    }
 };
 
 class TurkeyAdapter : public Duck
 {
 private:
-	std::unique_ptr<Turkey>& turkey;
+    std::unique_ptr<Turkey>& turkey;
 public:
-	TurkeyAdapter(std::unique_ptr<Turkey>& turkey) : turkey(turkey) {}
+    TurkeyAdapter(std::unique_ptr<Turkey>& turkey) : turkey(turkey) {}
 
-	virtual void fly() override
-	{
-		for (int i = 0; i < 5; i++)
-			turkey->fly();
-	}
+    virtual void fly() override
+    {
+        for (int i = 0; i < 5; i++)
+            turkey->fly();
+    }
 
-	virtual void quack() override
-	{
-		turkey->gobble();
-	}
+    virtual void quack() override
+    {
+        turkey->gobble();
+    }
 };
 
 void testDuck(const std::unique_ptr<Duck>& duck)
 {
-	duck->fly();
-	duck->quack();
+    duck->fly();
+    duck->quack();
 }
 
 int main()
 {
-	std::unique_ptr<Duck> mallardDuck = std::make_unique<MallardDuck>();
-	testDuck(mallardDuck);
+    std::unique_ptr<Duck> mallardDuck = std::make_unique<MallardDuck>();
+    testDuck(mallardDuck);
 
-	std::unique_ptr<Turkey> wildTurkey = std::make_unique<WildTurkey>();
-	std::unique_ptr<Duck> turkeyAdpter = std::make_unique<TurkeyAdapter>(wildTurkey);
-	testDuck(turkeyAdpter);
+    std::unique_ptr<Turkey> wildTurkey = std::make_unique<WildTurkey>();
+    std::unique_ptr<Duck> turkeyAdpter = std::make_unique<TurkeyAdapter>(wildTurkey);
+    testDuck(turkeyAdpter);
 }
